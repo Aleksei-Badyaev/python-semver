e3a8ce18f59e798ea100f98e1e4654eb53914211
Aleksei Badyaev <aleksei.badyaev@gmail.com>
2020-01-14 19:48:04 +0700

    Fix jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -7,9 +7,12 @@ pip install -e .[testing] stdeb3
 python setup.py --command-packages=stdeb3.command bdist_deb
 rm ./*.tar.gz
 python setup.py bdist_wheel
-mkdir -p build/
 VERSION=$(python setup.py --version)
 echo "${VERSION}" > ./VERSION
+if [[ -z "$BRANCH" ]]; then
+    BRANCH="$(git rev-parse --abbrev-ref HEAD | sed -e 's%/%-%g')"
+fi
+mkdir -p build
 sed\
  -e "s/VERSION/${VERSION}/g"\
  -e "s/BRANCH/${BRANCH}/g"\
